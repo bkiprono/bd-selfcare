569257edc7f150ea86e782b876b4fcf9fc292d55
Eng. Brian Koech <89153335+bkiprono@users.noreply.github.com>
2026-01-24 05:54:27 +0300

    refactor: clean up the mobile app

--- lib/core/navigation/back_gesture_detector.dart
+++ lib/core/navigation/back_gesture_detector.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'dart:math' as math;
 
 /// A full-screen back gesture detector for iOS-like navigation.
 /// Allows swiping from anywhere on the screen to go back.
